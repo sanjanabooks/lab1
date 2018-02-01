5ea435297b4415a69c556e69d2c096fd4ab5001c
Sanjana Narayanan <sanjananarayanan@Sanjanas-MacBook-Pro.local>
2018-02-01 16:00:23 -0500

    end of class again

--- lab1.ml
+++ lab1.ml
@@ -179,7 +179,7 @@ let rec square_all (lst : int list) : int list =
   | [] -> []
   | head :: tail -> head * head :: square_all(tail) ;;
 
-let exercise6 = square_all[3; 4; 5] ;;
+let exercise6 = square_all ([3; 4; 5]) ;;
 
 (*......................................................................
 Exercise 7: Define a recursive function that sums an integer
